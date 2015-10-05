a3ac415e96115e5d291be347c172c93644a8523e
Noah <noah@noahveltman.com>
2015-10-05 16:06:49 -0400

    Adding --yes for spotify install

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ cp .bash_aliases ~/
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt-get --yes install spotify-client
 
 # Turn off online searching in Dash
 gsettings set com.canonical.Unity.Lenses remote-content-search none
